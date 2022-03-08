0105665d07dc1c95473ccdd092b77258dc14354c
hkantare <hkantare@in.ibm.com>
2022-03-08 10:41:47 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "web" {
   provisioner "local-exec" { 
-      command = "ssh -i ~/.ssh/id_rsa -o ProxyCommand='ssh -i ~/.ssh/id_rsa -W %h:%p root@52.118.184.108' root@172.16.0.4;ls" 
+      command = "ssh -i ~/.ssh/id_rsa -o ProxyCommand='ssh -i ~/.ssh/id_rsa -W %h:%p root@52.118.190.71' root@172.16.0.4;ls" 
       }
 }
